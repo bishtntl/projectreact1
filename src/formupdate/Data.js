4563dbd719ff2810661976b79ada270664c992b1
bishtntl <bishtntl04@gmail.com>
2023-09-02 02:31:11 +0530

    okk

---
+++ src/formupdate/Data.js
@@ -0,0 +1,45 @@
+import { Component } from "react";
+
+
+class DataCompo extends Component{
+    constructor(props){
+        super(props);
+        console.log(this.props.value)
+    }
+    
+    render(){
+        return(
+            <>
+            <div className="global">
+                {this.props.value.map((item,index)=>{
+                    return(
+                        <div  >
+                            <h1 key={index} className="one">
+                                Name:{item.name}|
+                                Name:{item.department}|
+                                Name:{item.rating}|
+                                
+                            </h1>
+                           
+
+                        </div>
+                    )
+
+                })}
+               
+            </div>
+            <button onClick={this.props.ChangeFunc} className="bntgoback">Go Back</button>
+            </>
+        )
+    }
+}
+    
+export default DataCompo;
+
+
+
+
+
+
+
+  
---
+++ src/formupdate/Form.js
@@ -0,0 +1,69 @@
+import React,{Component} from 'react'
+import './style.css'
+import DataCompo from './Data';
+
+class StructureCompo extends Component{
+    constructor(){
+        super();
+        this.state={
+            Name:" ",
+            Department:" ",
+            Rating:" ",
+            Click:true,
+            subarr:[]
+        }
+    }
+    changedata =(e)=>{
+        this.setState({[e.target.name]:e.target.value})
+        console.log(e.target.value)
+      
+    }
+    clickme=(a)=>{
+        a.preventDefault()
+        const  objtem={
+            name:this.state.Name,
+            department:this.state.Department,
+            rating:this.state.Rating
+            }
+            this.state.subarr.push(objtem)
+            this.setState({
+                subarr: this.state.subarr,
+                Name:" ",
+            Department:" ",
+            Rating:" ",
+            Click:false
+            })
+
+    }
+    ToggleFunc=()=>{
+        this.setState({
+            Click: !this.state.Click
+        })
+    }
+    render(){
+        return(
+            <>
+           
+        
+            <h1 className='heading'>Employee FeeDBack Form</h1>
+
+
+            {this.state.Click? <form> 
+              <label htmlFor="name"><b style={{fontSize:'1.5em'}}>Name :</b></label>
+                <input type='text' name='Name' id='name' placeholder='Enter Your Name' value={this.state.Name} autoComplete='off' required className='round' onChange={this.changedata}/><br/><br/>
+                <label htmlFor="depart"><b style={{fontSize:'1.5em'}}>Department :</b></label>
+                <input type='text' name='Department' id='depart' placeholder='Enter Your Department' value={this.state.Department} autoComplete='off' required className='round' onChange={this.changedata}/><br/><br/>
+                <label htmlFor="rate"><b style={{fontSize:'1.5em'}}>Rating :</b></label>
+                <input type='text' name='Rating' placeholder='Give Your Rating' value={this.state.Rating}  autoComplete='off' required  className='round' onChange={this.changedata}/><br/><br/>
+
+             <button onClick={this.clickme} className='btn'>Submit</button>
+
+              </form> 
+             :<DataCompo value={this.state.subarr} ChangeFunc={this.ToggleFunc}/>}
+
+          
+            </>
+        )
+    }
+}
+export default StructureCompo;
